79186baf8252e025174ef94a02b59ba6e73861e8
吕星辰 <xingchen.lv@cisdi.com.cn>
2020-12-03 09:46:07 +0800

    修改刷新方式

--- core/base/src/main/java/com/lxc/base/binding/BaseBinding.kt
+++ core/base/src/main/java/com/lxc/base/binding/BaseBinding.kt
@@ -65,7 +65,7 @@ fun <T> setServerDataRecyclerView(
                 } else {
                     adapter.loadMoreComplete()
                 }
-                adapter.notifyItemChanged(adapter.data.size - serverData.data.size - 1)
+                adapter.notifyDataSetChanged()
             }
         }
         BaseRecyclerViewResult.DEAL_TYPE_REFRESH -> {
