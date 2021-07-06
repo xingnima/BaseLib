9c1841abc84a7316cc588c0e9b4463748cd461e0
吕星辰 <562566344@qq.com>
2021-07-06 15:54:04 +0800

    修改gradle版本

--- app/src/main/java/com/lxc/lxcbaselib/MainActivity.kt
+++ app/src/main/java/com/lxc/lxcbaselib/MainActivity.kt
@@ -2,6 +2,7 @@ package com.lxc.lxcbaselib
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.lxcbaselib.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
